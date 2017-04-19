89e65400988e44ad052b11d41cdc3fdd439f4d38
zhengkai <568230747@qq.com>
2017-04-19 23:36:26 +0800

    -a

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,7 +1,47 @@
-/**
- * 
- * @authors Your Name (you@example.org)
- * @date    2017-04-19 17:55:47
- * @version $Id$
- */
+function carousel(parentObj,childObj,carouselIndex){
+	var winWidth=null;
+	var imgLength=null;
+	var time=null;
+	var Index=0;
+	var Span="<span></span>";
+	var spanAll="";
+	reset();
+	for (var i = 0; i < imgLength; i++) {
+		spanAll+=Span;
+	};
+	$(carouselIndex).html(spanAll);
+	$(carouselIndex).css({"margin-left":-($(carouselIndex).width()-15)/2})
+	$(carouselIndex).find("span").eq(Index).addClass('active');
+	$(carouselIndex).on('click', 'span', function(event) {
+		Index=$(this).index();
+		$(parentObj).stop(true).animate({left:-winWidth*Index}, 1000);
+		$(carouselIndex).find("span").eq(Index).addClass('active').siblings('span').removeClass('active');
+		event.preventDefault();
+		/* Act on the event */
+	});
+	time=setInterval(carouselAnimate,3000);
+	function carouselAnimate(){
+		$(carouselIndex).find("span").eq(Index).addClass('active').siblings('span').removeClass('active');
+		$(parentObj).stop(true).animate({left:-winWidth*Index}, 1000);
+		Index++;
+		if(Index==imgLength){
+			Index=0;
+		}
+	};
+	$(window).resize(function(event) {
+		reset();	
+		$(parentObj).css({left:-winWidth});
+	});
+	function reset(){
+		winWidth=$(window).outerWidth()>1200?$(window).outerWidth():1200;
+		imgLength=$(childObj).length;
+		console.log(winWidth)
+		$(parentObj).width(imgLength*winWidth);
+	};
+	$(parentObj).parent().hover(function() {
+		clearInterval(time)
+	}, function() {
+		time=setInterval(carouselAnimate,3000)
+	});
+}
 
